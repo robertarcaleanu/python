50cca0778f28845e876e09f85212b497abe644f0
Robert Arcaleanu <112280290+robertarcaleanu@users.noreply.github.com>
2023-02-25 19:41:05 +0000

    [01_Introduction] - completed

--- 01_Introduction.py
+++ 01_Introduction.py
@@ -1,12 +1,87 @@
-import pandas as pd
-import numpy as np
+# Concatenate variables
+num = 12
+name = 'Rob'
 
-# x = np.array([1, 2, 3])
-y = np.array([1, 2, 3])
-print(y**2)
+print('My number is {} and my name is {}'.format(num, name))
 
-for i in range(5):
-    print (i)
+# Select character from a string
+print(name[0])
+print(name[1:3])
+print(name[1:])
 
+# Lists
+my_list = ['a', 'b', 'c']
+print(my_list[0])
+my_list.append('d')
+print(my_list[2:])
 
-print(i)
+# dictionary
+
+d = {'key1': 'value', 'key2': 123}
+print(d['key1'])
+
+d1 = {'k1':{'innerKey':[1, 2, 3]}}
+print(d1['k1']['innerKey'])
+
+# Tuple
+t = (0, 1, 2) # we can't reasign value
+print(t)
+print(t[0])
+
+s = {1, 2, 3}
+s.add(5)
+print(s)
+
+# Conditionals
+
+if 1 == 2:
+    print('one')
+elif 3 == 3: 
+    print('middle')
+else:
+    print('last')
+
+# Loop
+seq = [1, 2, 3, 4]
+
+for i in seq:
+    print(i)
+
+out = []
+
+for i in range(10):
+    out.append(i**2)
+
+print(out)
+
+print([i**2 for i in range(10)])
+
+# Functions
+def my_func(myParam):
+    print(myParam)
+
+my_func('This is a function!')
+
+# Map
+
+def times2 (var):
+    return var**2
+
+seq = range(10)
+print('result obtained with map')
+print(list(map(times2, seq)))
+print(list(map(lambda num: num**2, seq))) # we can also use lambda
+
+# filter 
+
+print(list(filter(lambda num: num%2 == 0, seq)))
+
+# methods
+s = 'My name is Robert'
+print(s.lower())
+print(s.upper())
+print(s.split())
+
+# in operator
+print('x' in [1, 2, 3])
+
